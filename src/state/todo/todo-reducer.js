9ab93e28feadaa8b85e8af82d412aa56f02c5e89
Rivka Stender <rstender@student.touro.edu>
2023-03-10 02:03:48 -0500

    edited the remove function to work from todo-reducer.js

--- src/state/todo/todo-reducer.js
+++ src/state/todo/todo-reducer.js
@@ -3,6 +3,7 @@ import { cloneDeep } from "lodash";
 export const TodoActions = {
   ADD: "ADD",
   TOGGLE: "TOGGLE",
+  REMOVE: "REMOVE",
 };
 
 export const todoReducer = (state, action) => {
@@ -14,6 +15,17 @@ export const todoReducer = (state, action) => {
       let newTodos = cloneDeep(state.todos);
       const updateTodo = newTodos.find((x) => x.title === action.todo.title);
       updateTodo.isComplete = !updateTodo.isComplete;
+      return {
+        todos: [...newTodos],
+      };
+    }
+    case TodoActions.REMOVE: {
+      const newTodos = cloneDeep(state.todos);
+      const findTodo = newTodos.find((x) => x.title === action.todo.title);
+      let index = newTodos.indexOf(findTodo);
+      if (index !== -1) {
+        newTodos.splice(index, 1);
+      }
       return {
         todos: [...newTodos],
       };
